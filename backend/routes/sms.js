7c34e4bc8be007657e81a20ea6f73c522d8608d4
Joan Kurui( software developer) <135380468+Joankurui@users.noreply.github.com>
2025-12-16 23:36:28 +0300

    Delete backend/routes directory

--- backend/routes/sms.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const analyzeController = require('../controllers/analyzeController');
-
-router.post('/analyze', analyzeController.analyzeSMS);
-
-module.exports = router;
